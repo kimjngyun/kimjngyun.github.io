6c325877ddb5c5d627e87e9eca978e6df40f2742
kimjngyun <sanbada79@naver.com>
2022-06-05 21:06:02 +0900

    config gtag

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -44,12 +44,9 @@ const config = {
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
         },
-        googleAnalytics: {
-          trackingID: "G-BYCJ5ET6ZH",
-          anonymizeIP: true,
-        },
         gtag: {
           trackingID: "G-BYCJ5ET6ZH",
+          anonymizeIP: false,
         },
       }),
     ],
