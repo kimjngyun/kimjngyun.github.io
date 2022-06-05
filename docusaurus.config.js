18ac1f21f3ae42b86acc75db3f81de7066e46ad0
kimjngyun <sanbada79@naver.com>
2022-06-05 19:33:48 +0900

    add test article and rm docs files

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -32,13 +32,7 @@ const config = {
       "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
-        docs: {
-          sidebarPath: require.resolve("./sidebars.js"),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
-        },
+        docs: false,
         blog: {
           routeBasePath: "/",
           showReadingTime: true,
